823c61458b5bb90a2f68cf03df1144e7ee446990
James <blackburnfjames@gmail.com>
2020-03-19 17:05:48 +0000

    Updated to module format

--- __main__.py
+++
@@ -1,7 +0,0 @@
-from .logger import Logger
-from .videocardbenchmark import videocardbenchmark_scraper_mega
-
-if __name__ == "__main__":
-    scraper = videocardbenchmark_scraper_mega()
-    scraper.refresh_all()
-    print(scraper._gpus)
