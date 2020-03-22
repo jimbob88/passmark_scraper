7a3ebe419507341dd5469328c47af8ea9a1bb35d
James <blackburnfjames@gmail.com>
2020-03-22 11:59:31 +0000

    Updated setup.py to 0.0.2

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="passmark_scraper-jimbob88", # Replace with your own username
-    version="0.0.1",
+    name="passmark_scraper-jimbob88",
+    version="0.0.2",
     author="James Blackburn",
     author_email="blackburnfjames@gmail.com",
     description="A ( for personal use only ) scraper for CPU + GPU data from passmark.com",
